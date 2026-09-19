edb984adf63a2c60f8210600df2dd0e33a8e3a25
tenkouruyu <tenkouryu.mtg@gmail.com>
2026-09-19 10:59:31 +0900

    コマンドの命名ルール変更に伴う修正
    (cherry picked from commit 98f9239a7f92155e800d8af535a6199694ce3498)

--- event/on_message/message_comand/member_get_list.py
+++ event/on_message/message_comand/member_get_list.py
@@ -6,7 +6,7 @@ import function.send_message as send_message
 async def main(client, message):
     if message.content.partition(' ')[2].strip() == '-h':
         await message.channel.send(
-            '/member_list\n'
+            '/member_get_list\n'
             'サーバーのメンバー一覧を CSV ファイルで取得します。'
         )
         return
