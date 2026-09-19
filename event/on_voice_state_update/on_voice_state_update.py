58d7efcfb7b180ea4bf6a41024b62111a07f909e
tenkouruyu <tenkouryu.mtg@gmail.com>
2026-09-20 02:54:26 +0900

    ボイスチャットのリダイレクト先を修正

--- event/on_voice_state_update/on_voice_state_update.py
+++ event/on_voice_state_update/on_voice_state_update.py
@@ -3,7 +3,6 @@ import function.send_message as send_message
 
 async def on_voice_state_update_main(
     client: discord.Client,
-    channel_id: int,
     member: discord.Member,
     before: discord.VoiceState,
     after: discord.VoiceState,
@@ -11,7 +10,15 @@ async def on_voice_state_update_main(
     print(f'{member}のボイス状態が変更されました')
     # メンバーがボイスチャンネルに参加した場合
     if before.channel is None and after.channel is not None:
-        await send_message.send_message_to_channel(client, channel_id, f'{member.display_name}さんがボイスチャンネルに参加しました！')
+        await send_message.send_message_to_channel(
+            client,
+            after.channel.id,
+            f'{member.display_name}さんがボイスチャンネルに参加しました！',
+        )
     # メンバーがボイスチャンネルから退出した場合
     elif before.channel is not None and after.channel is None:
-        await send_message.send_message_to_channel(client, channel_id, f'{member.display_name}さんがボイスチャンネルから退出しました！')
+        await send_message.send_message_to_channel(
+            client,
+            before.channel.id,
+            f'{member.display_name}さんがボイスチャンネルから退出しました！',
+        )
