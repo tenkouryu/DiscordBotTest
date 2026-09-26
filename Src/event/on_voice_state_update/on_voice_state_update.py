c6961f38c916dd5497f2180255595aa46318001c
tenkouruyu <tenkouryu.mtg@gmail.com>
2026-09-26 19:25:55 +0900

    サンプル機能をコードだけ残してカット
    (cherry picked from commit 3b31a6ff2f46cb9c4481f56ad66a9f0edbf893a3)

--- Src/event/on_voice_state_update/on_voice_state_update.py
+++ Src/event/on_voice_state_update/on_voice_state_update.py
@@ -15,17 +15,19 @@ async def on_voice_state_update_main(
     after: discord.VoiceState,
 ) -> None:
     print(f'{member}のボイス状態が変更されました')
-    # メンバーがボイスチャンネルに参加した場合。
-    if before.channel is None and after.channel is not None:
-        await send_message.send_message_to_channel(
-            client,
-            after.channel.id,
-            f'{member.display_name}さんがボイスチャンネルに参加しました！',
-        )
-    # メンバーがボイスチャンネルから退出した場合。
-    elif before.channel is not None and after.channel is None:
-        await send_message.send_message_to_channel(
-            client,
-            before.channel.id,
-            f'{member.display_name}さんがボイスチャンネルから退出しました！',
-        )
+    if 0:
+        # メンバーがボイスチャンネルに参加した場合。
+        if before.channel is None and after.channel is not None:
+            await send_message.send_message_to_channel(
+                client,
+                after.channel.id,
+                f'{member.display_name}さんがボイスチャンネルに参加しました！',
+            )
+        # メンバーがボイスチャンネルから退出した場合。
+        elif before.channel is not None and after.channel is None:
+            await send_message.send_message_to_channel(
+                client,
+                before.channel.id,
+                f'{member.display_name}さんがボイスチャンネルから退出しました！',
+            )
+            return
